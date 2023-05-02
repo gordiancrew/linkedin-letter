7a49dadc7bf36ee9c30260641b5549127f1a50a4
Uladzimir Kazantsau <gordiancrew@gmail.com>
2023-05-02 15:05:10 +0300

    refactor: style

--- src/components/generator-text/generator-text.tsx
+++ src/components/generator-text/generator-text.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import 'regenerator-runtime/runtime';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+// import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import './generator-text.css';
 function GeneratorText() {
-  const { transcript } = useSpeechRecognition();
-  function startVoice() {
-    SpeechRecognition.startListening();
-  }
-  function stopVoice() {
-    SpeechRecognition.stopListening();
-  }
-  function voice() {
-    const text = transcript;
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.voice = window.speechSynthesis.getVoices()[1];
-    window.speechSynthesis.speak(utterance);
-  }
+  // const { transcript } = useSpeechRecognition();
+  // function startVoice() {
+  //   SpeechRecognition.startListening();
+  // }
+  // function stopVoice() {
+  //   SpeechRecognition.stopListening();
+  // }
+  // function voice() {
+  //   const text = transcript;
+  //   const utterance = new SpeechSynthesisUtterance(text);
+  //   utterance.voice = window.speechSynthesis.getVoices()[1];
+  //   window.speechSynthesis.speak(utterance);
+  // }
 
   return (
     <div className="generator-text-wrapper">
